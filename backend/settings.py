c27091b3b7a7d8506626291274e3a16a20ad640c
dhairya2707 <60231527+dhairya2707@users.noreply.github.com>
2023-12-10 15:22:23 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 django_heroku.settings(locals())
-
+del DATABASES['default']['OPTIONS']['sslmode']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
